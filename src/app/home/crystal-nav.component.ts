86798f074edfafa45444d8c5596919676206555c
erya <hang3126@sina.com>
2017-02-21 17:37:33 +0800

    add jquery.ripple plugin for crystal nav component

--- src/app/home/crystal-nav.component.ts
+++ src/app/home/crystal-nav.component.ts
@@ -2,28 +2,17 @@
  * Created by qhyang on 2017/2/17.
  */
 
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
     selector: "crystal-nav",
-    template: `
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-    `,
+    template: require("./crystal-nav.component.pug")(),
     styles: [require("./crystal-nav.component.scss")]
 })
-export class CrystalNavComponent{ }
+export class CrystalNavComponent implements OnInit {
+    ngOnInit() {
+        let jquery = require("jquery");
+        require("jquery.ripples");
+        jquery(".ripple").ripples();
+    }
+}
